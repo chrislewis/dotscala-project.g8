bb18428de4065a8c145a6d9721331052aeaaed3b
Chris Lewis <clewis@novus.com>
2012-04-01 13:25:13 -0400

    single project

--- src/main/g8/project/$name;format="Camel,word"$.scala
+++ src/main/g8/project/$name;format="Camel,word"$.scala
@@ -1,24 +1,20 @@
 import sbt._
 import Keys._
 
-object $name;format="Camel,word"$ extends sbt.Build {
-
-  lazy val root =
-    project(id = "$name;format="normalize"$-all",
-            base = file(".")) aggregate(core)
-
+object $name;format="Camel,word"$Build extends sbt.Build {
+  
   lazy val core =
-    project(id = "$name;format="normalize"$-core",
-            base = file("$name$-core"),
-            settings = Seq(
+    Project(id = "$name;format="normalize"$-core",
+            base = file("."),
+            settings = Project.defaultSettings ++ Seq(
+              organization := "$organization$",
+              version := "$version$",
+              scalaVersion := "2.9.1",
+              crossScalaVersions := Seq("2.8.1, 2.9.0, 2.9.0-1"),
+              initialCommands := "import $organization$.$name;format="normalize,word"$._",
               libraryDependencies <++= scalaVersion (v => Seq(
                 Shared.specsDep(v) % "test"
             ))))
-            
-  def project(id: String, base: File, settings: Seq[Project.Setting[_]] = Nil) =
-    Project(id = id,
-            base = base,
-            settings = Project.defaultSettings ++ Shared.settings ++ settings)
 }
 
 object Shared {
@@ -31,13 +27,5 @@ object Shared {
       case "2" :: "9" :: _ => "org.scala-tools.testing" %% "specs" % "1.6.8"
       case _ => sys.error("Specs not supported for scala version %s" format sv)
     }
-  
-  val settings = Seq(
-    organization := "$organization$",
-    version := "$version$",
-    scalaVersion := "2.9.1",
-    crossScalaVersions := Seq("2.8.1, 2.9.0, 2.9.0-1"),
-    initialCommands := "import $organization$.$name;format="normalize,word"$._"
-  )
   
 }
